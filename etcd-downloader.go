4dd4a6fe8dfd3e734b26c3845e718e5098cd0e3f
katsunori sekine <katsutomu0124@gmail.com>
2016-11-23 02:46:48 +0900

    Download & Locate

--- etcd-downloader.go
+++ etcd-downloader.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"etcd-downloader/locater"
+	"etcd-downloader/provider"
+	"flag"
+
+	"github.com/labstack/gommon/log"
+)
+
+var endpoint string
+var dir string
+var ext string
+var outpath string
+var outfile string
+
+func init() {
+	flag.StringVar(&endpoint, "endpoint", "http://localhost:2379", "etcd or consol")
+	flag.StringVar(&dir, "dir", "/config", "config file path")
+	flag.StringVar(&ext, "ext", "json", "file ext")
+	flag.StringVar(&outpath, "opath", "./", "output location")
+	flag.StringVar(&outfile, "filename", "config.json", "output filename")
+}
+
 func main() {
+	flag.Parse()
+	b, err := provider.ReadRemoteConfig("etcd", endpoint, dir, ext)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err := locater.LocateFile(outpath, outfile, b); err != nil {
+		log.Error(err)
+	}
 }
